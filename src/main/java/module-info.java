1cd0628588b90a77e254fb5ab61fc7a6f4bb89b5
Gerrit Grunwald <han.solo@mac.com>
2021-12-29 12:54:58 +0100

    Initial code commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module eu.hansolo.fx.heatmap {
+    // Java
+    requires java.base;
+
+    // Java-FX
+    requires transitive javafx.base;
+    requires transitive javafx.graphics;
+    requires transitive javafx.controls;
+    requires transitive javafx.swing;
+
+    // 3rd Party
+    requires transitive eu.hansolo.toolbox;
+    requires transitive eu.hansolo.toolboxfx;
+
+    exports eu.hansolo.fx.heatmap;
+}
